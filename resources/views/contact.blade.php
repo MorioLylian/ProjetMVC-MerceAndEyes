38b282cf80e5965aedd43f00b9c78508937128f7
Lucas Maillard <46894925+LucasMaillard@users.noreply.github.com>
2020-02-12 15:10:08 +0100

    Ajout page contact
    Ajout route pouyr contact et page contact

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>MerchAndEyes</title>
+
+
+        <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet"> 
+
+        <style>
+            
+
+            body { margin:0px !important;}
+
+            .header {
+                overflow: hidden;
+                padding: 20px 10px;
+                border-color : #8d3c93;
+                background-color: white;
+                box-shadow: 2px 2px 6px black;
+                border-style: solid;
+                border-width: 2px;
+                margin:0px !important;
+                }
+
+            .header a {
+                float: left;
+                color: #8d3c93;
+                padding: 12px;
+                text-decoration: none;
+                font-size: 18px;
+                line-height: 25px;
+                border-radius: 4px;
+            }
+
+            .header a.logo {
+                font-size: 25px;
+                font-weight: bold;
+            }
+
+            .oeil {
+                width:10%;
+                min-width:30px;
+            }
+
+            .logo :hover{
+                cursor:pointer;
+            }
+
+            .header-right a:hover {
+                text-decoration:underline;
+                cursor:pointer;
+            }
+
+            .header a.active {
+                background-color: #8d3c93;
+                color: white;
+            }
+
+            .header-right {
+                float: right;
+            }
+
+            @media screen and (max-width: 500px) {
+                .header a {
+                    float: none;
+                    display: block;
+                    text-align: left;
+                }
+                .header-right {
+                    float: none;
+                }
+            }
+
+
+
+
+            footer{
+                overflow: hidden;
+                padding: 20px 10px;
+                border-color : #8d3c93;
+                background-color: white;
+                box-shadow: 2px -2px 3px black;
+                border-style: solid;
+                border-width: 2px;
+                margin:0px !important;
+                
+            }
+
+            footer div{
+                width:33%;
+                text-align:center; 
+            }
+
+            .footer{
+                display:flex;
+                width:70%;
+                margin-left:15%;
+            }
+
+            .violet{
+                color:#8d3c93;
+                cursor:pointer;
+            }
+
+            footer a:hover{
+                text-decoration:underline;
+            }
+
+
+        </style>
+    </head>
+    <body>
+        <header class="header">
+            
+            <a class="logo"><img class="oeil" src="images/oeil.png" alt="Logo de notre site"/> MerchAndEyes </a>
+            <div class="header-right">
+                <a >Accueil</a>
+                <a >Gallerie</a>
+                <a class="active">Contact</a>
+            </div>
+        </header>
+        <div class="container-contact100">
+		<div class="contact100-map" id="google_map" data-map-x="40.722047" data-map-y="-73.986422" data-pin="images/icons/map-marker.png" data-scrollwhell="0" data-draggable="1"></div>
+
+		<div class="wrap-contact100">
+			<form class="contact100-form validate-form">
+				<span class="contact100-form-title">
+					Contact Us
+				</span>
+
+				<div class="wrap-input100 validate-input" data-validate="Name is required">
+					<input class="input100" type="text" name="name" placeholder="Full Name">
+					<span class="focus-input100-1"></span>
+					<span class="focus-input100-2"></span>
+				</div>
+
+				<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+					<input class="input100" type="text" name="email" placeholder="Email">
+					<span class="focus-input100-1"></span>
+					<span class="focus-input100-2"></span>
+				</div>
+
+				<div class="wrap-input100 validate-input" data-validate = "Message is required">
+					<textarea class="input100" name="message" placeholder="Your Message"></textarea>
+					<span class="focus-input100-1"></span>
+					<span class="focus-input100-2"></span>
+				</div>
+
+				<div class="contact100-form-checkbox">
+					<input class="input-checkbox100" id="ckb1" type="checkbox" name="copy-mail">
+					<label class="label-checkbox100" for="ckb1">
+						Send copy to my-email
+					</label>
+				</div>
+
+				<div class="container-contact100-form-btn">
+					<button class="contact100-form-btn">
+						Send Email
+					</button>
+				</div>
+			</form>
+		</div>
+	</div>   
+        <footer>
+            <div class="footer">
+                <div>
+                    <p>Nos Résaux</p>
+                </div>
+                    
+                <div>
+                    <p>Naviguer vers <a class="violet"> Accueil </a><a class="violet"> Gallerie </a><a class="violet"> Contact </a></p>
+                </div>
+                    
+                <div>
+                    <p>Passer en Anglais</p>
+                </div>
+            </div>
+        </footer>
+    </body>
+</html>
