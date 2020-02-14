bd16c2aa6a10a5567af0d5bb1c6878c5f04b18dd
MorioLylian <47317919+MorioLylian@users.noreply.github.com>
2020-02-14 09:57:58 +0100

    changement de l'affichage du retour de mail

--- resources/views/MailContact.blade.php
+++ resources/views/MailContact.blade.php
@@ -2,14 +2,22 @@
 <html lang="fr">
   <head>
     <meta charset="utf-8">
+
+    <style>
+
+    .bold{
+      color:#8d3c93;
+      font-weight: bold;
+    }
+    </style>
   </head>
   <body>
-    <h2>Prise de contact sur mon beau site</h2>
-    <p>Réception d'une prise de contact avec les éléments suivants :</p>
+    <h2 class="bold">QUELQU'UN VOUS A ENVOYÉ UN MESSAGE À PARTIR DU SITE</h2>
+    <p class="bold">Voici les informations</p>
     <ul>
-      <li><strong>Nom</strong> : {{ $contact['name'] }}</li>
-      <li><strong>Email</strong> : {{ $contact['email'] }}</li>
-      <li><strong>Message</strong> : {{ $contact['message'] }}</li>
+      <li><span class="bold">Nom de l'expéditeur:</span> {{ $contact['name'] }}</li>
+      <li><span class="bold">Email de l'expéditeur:</span> {{ $contact['email'] }}</li>
+      <li><span class="bold">Le message de l'expéditeur:</span> {{ $contact['message'] }}</li>
     </ul>
   </body>
 </html>
